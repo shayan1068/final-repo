3aa747497f0c3af60174214137916142253845ab
shayan1068 <160053138+shayan1068@users.noreply.github.com>
2024-11-27 01:49:21 +0500

    Update profile-db.tsx

--- frontend0/src/webpages/profile/profile-db.tsx
+++ frontend0/src/webpages/profile/profile-db.tsx
@@ -3,7 +3,7 @@ export const deleteUserPostedPost = async(serviceId:string,userId:string)=>{
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: `http://localhost:3000/api/detete/userPost/${serviceId}/user/${userId}`,
+        url: `https://final-repo-k8c4.onrender.com/api/detete/userPost/${serviceId}/user/${userId}`,
       };
       
       try {
